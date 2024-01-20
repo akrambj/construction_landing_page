2c9c60dbcd898502acd85e70b05f0e6a0639f90d
akrambj <bedjaouiakram.93@gmail.com>
2024-01-20 18:23:59 +0000

    header mobile

---
+++ src/components/data/Socials.jsx
@@ -0,0 +1,11 @@
+import { FaFacebookF, FaInstagram, FaLinkedinIn } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+
+const Socials = [
+  { icon: <FaTwitter />, link: "https://twitter.com/x" },
+  { icon: <FaInstagram />, link: "https://www.instagram.com/" },
+  { icon: <FaLinkedinIn />, link: "https://www.linkedin.com/" },
+  { icon: <FaFacebookF />, link: "https://www.facebook.com/" },
+];
+
+export default Socials;
--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,5 +1,91 @@
+import { useState } from "react";
+import logo from "../../assets/imgs/logo.svg";
+import logoWhite from "../../assets/imgs/logoWhite.svg";
+import Socials from "../data/Socials";
+
 const Header = () => {
-  return <header className="">header</header>;
+  const [menuToggle, setMenuToggle] = useState(false);
+  const [selectedLink, setSelectedLink] = useState(0);
+
+  const menuItems = [
+    { name: "à Propos", link: "/" },
+    { name: "NOS SERVICES", link: "#services" },
+    { name: "Testemonials", link: "#agency" },
+    { name: "Contacts", link: "#values" },
+  ];
+
+  const toggleMenu = () => {
+    setMenuToggle((prevMenuToggle) => !prevMenuToggle);
+  };
+  return (
+    <header className="w-screen fixed top-0 left-0 flex items-center justify-between py-4 px-7 ">
+      <div className={`logo w-[60%] relative z-40`}>
+        <img
+          className="w-full h-full object-cover duration-300 "
+          src={menuToggle ? logoWhite : logo}
+          alt="logo"
+        />
+      </div>
+      <div className="menu">
+        <div
+          className={`menuicon z-50 ${menuToggle ? "openMenu" : ""}`}
+          onClick={toggleMenu}
+        >
+          <span className="line-1 bg-yellow-primary"></span>
+          <span className="line-2 bg-yellow-primary"></span>
+          <span className="line-3 bg-yellow-primary"></span>
+        </div>
+        <div
+          className={`absolute top-0 h-screen w-screen overflow-hidden ${
+            menuToggle ? " right-0  menu-bg" : "-right-[100vw]"
+          } transition-all ease-in duration-300`}
+        >
+          <div className="overlay absolute left-0 top-0 right-0 bottom-0 z-20"></div>
+          <div className="w-[80%] mx-auto h-[90%] pt-14 mt-10  flex flex-col justify-between text-white z-30  relative">
+            <div className="flex flex-col gap-12">
+              <h2 className="border-l-4 border-green-primary pl-2 uppercase w-[90%] text-xl font-bold">
+                <span className="text-yellow-primary">AVEC VOUS</span> JUSQU’A
+                LA DERNIèRE <span className="text-green-primary">PIERRE</span>
+              </h2>
+              <ul className="flex flex-col gap-4 px-2">
+                {menuItems.map((item, index) => (
+                  <li
+                    className={`uppercase text-xl ${
+                      selectedLink === index
+                        ? "text-yellow-primary font-bold"
+                        : ""
+                    }`}
+                    key={index}
+                    onClick={() => {
+                      setMenuToggle(false);
+                      setSelectedLink(index);
+                    }}
+                  >
+                    {item.name}
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className="flex flex-col gap-2">
+              <h2 className="uppercase  font-semibold text-xl">Suivez-nous</h2>
+              <ul className="w-full flex items-center gap-2">
+                {Socials.map((media, index) => (
+                  <li key={index} className="">
+                    <a
+                      className="text-yellow-primary text-xl"
+                      href={media.link}
+                    >
+                      {media.icon}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
