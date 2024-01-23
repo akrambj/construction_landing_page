092115f64e098cd29c89572b56acbc018a657925
akrambj <bedjaouiakram.93@gmail.com>
2024-01-23 17:27:09 +0000

    hero section implementation

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import logo from "../../assets/imgs/logo.svg";
 import logoWhite from "../../assets/imgs/logoWhite.svg";
 import Socials from "../data/Socials";
 
@@ -10,7 +9,7 @@ const Header = () => {
 
   useEffect(() => {
     const handleScroll = () => {
-      if (window.scrollY >= 200) {
+      if (window.scrollY >= 20) {
         setScrollVersion(true);
       } else {
         setScrollVersion(false);
@@ -27,6 +26,7 @@ const Header = () => {
   console.log(scrollVersion);
 
   const menuItems = [
+    { name: "Accueil", link: "/" },
     { name: "à Propos", link: "/" },
     { name: "NOS SERVICES", link: "#services" },
     { name: "Testemonials", link: "#agency" },
@@ -37,15 +37,33 @@ const Header = () => {
     setMenuToggle((prevMenuToggle) => !prevMenuToggle);
   };
   return (
-    <header className="w-screen fixed top-0 left-0 flex items-center justify-between py-4 px-7  lg:px-32">
+    <header
+      className={`w-screen ${
+        scrollVersion ? "" : "bg-transparent"
+      } duration-300 fixed top-0 left-0 flex items-center justify-between py-4 px-7  md:px-24 lg:px-40 md:py-10`}
+    >
       <div
-        className={`logo w-[60%] xs:w-[50%] md:w-[30%] xl:w-[20%] relative z-40 ${
-          scrollVersion ? "opacity-0" : "opacity-100"
-        } duration-300`}
+        style={{
+          backdropFilter: "blur(10px)",
+          opacity: scrollVersion ? 0.4 : 0,
+        }}
+        className={`hidden md:block bg-black fixed w-[88%] py-8 left-[7%] rounded-[60px] ${
+          scrollVersion
+            ? "transition-opacity duration-300 ease-in-out"
+            : "opacity-0"
+        }`}
+      ></div>
+      <div
+        className={`logo  flex items-center justify-center  w-[60%] xs:w-[50%] md:w-[30%] xl:w-[20%] relative z-40  duration-300`}
       >
         <img
-          className="w-full h-full object-cover duration-300 "
-          src={menuToggle ? logoWhite : logo}
+          className="w-full h-full object-cover duration-300 md:hidden z-20 lg:cursor-pointer"
+          src={logoWhite}
+          alt="logo"
+        />
+        <img
+          className="w-full h-full object-cover duration-300 md:block hidden lg:cursor-pointer"
+          src={logoWhite}
           alt="logo"
         />
       </div>
@@ -59,17 +77,12 @@ const Header = () => {
           <span className="line-3 bg-yellow-primary"></span>
         </div>
         <div
-          className={`absolute top-0 h-screen w-screen overflow-hidden ${
-            menuToggle ? " right-0  menu-bg" : "-right-[100vw]"
+          className={`absolute top-0 h-screen w-screen overflow-hidden  ${
+            menuToggle ? "right-0  menu-bg" : "-right-[100vw]"
           } transition-all ease-in duration-300`}
         >
-          <div className="overlay absolute left-0 top-0 right-0 bottom-0 z-20"></div>
           <div className="w-[80%] mx-auto h-[90%] pt-14 mt-10  flex flex-col justify-between text-white z-30  relative">
             <div className="flex flex-col gap-12">
-              <h2 className="border-l-4 border-green-primary pl-2 uppercase w-[90%] text-xl font-bold">
-                <span className="text-yellow-primary">AVEC VOUS</span> JUSQU’A
-                LA DERNIèRE <span className="text-green-primary">PIERRE</span>
-              </h2>
               <ul className="flex flex-col gap-4 px-2">
                 {menuItems.map((item, index) => (
                   <li
@@ -107,18 +120,16 @@ const Header = () => {
           </div>
         </div>
       </div>
-      <nav
-        className={`items-center  hidden md:flex ${
-          scrollVersion
-            ? "bg-white shadow-xl py-3 px-5 rounded-lg"
-            : "bg-transparent py-0 px-0"
-        } duration-300`}
-      >
-        <ul className="flex items-center gap-5">
+      <nav className={`items-center  hidden md:flex  duration-300 z-20`}>
+        <ul className="flex items-center gap-5 ">
           {menuItems.map((item, index) => (
             <li
-              className={`cursor-pointer uppercase text-sm md:text-base xl:text-lg hover:text-yellow-primary duration-300 ${
-                selectedLink === index ? "text-yellow-primary font-bold" : ""
+              className={`cursor-pointer uppercase ${
+                scrollVersion ? "text-black" : "text-white"
+              } text-sm md:text-[11px] xl:text-lg hover:text-yellow-primary duration-300  ${
+                selectedLink === index
+                  ? "text-yellow-primary font-bold"
+                  : "text-white"
               }`}
               key={index}
               onClick={() => {
--- src/pages/SharedLayout.jsx
+++ src/pages/SharedLayout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/shared/Header";
 import Footer from "../components/shared/Footer";
+import CTABtn from "../components/shared/CTABtn";
 
 const SharedLayout = () => {
   return (
     <main className="w-screen min-h-screen flex flex-col justify-between overflow-x-hidden">
       <Header />
+      <CTABtn />
       <Outlet />
       <Footer />
     </main>
