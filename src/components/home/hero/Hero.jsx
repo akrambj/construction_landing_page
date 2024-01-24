cb7692a4e7ddff5d7fe434b4f3a241b67c2a65bb
akrambj <bedjaouiakram.93@gmail.com>
2024-01-24 22:46:03 +0000

    header and hero

--- src/components/home/hero/Hero.jsx
+++ src/components/home/hero/Hero.jsx
@@ -1,20 +1,37 @@
+import { motion } from "framer-motion";
+
 const Hero = () => {
   return (
-    <section className="w-full h-screen overflow-hidden phone-bg  md:px-40">
-      <div className=" h-full flex flex-col  items-center justify-end pb-10 text-center gap-3 z-20 lg:gap-5  text-white w-[85%] mx-auto md:mx-0 md:justify-center md:items-start md:text-left  lg:w-[50%]">
-        <h1 className="text-2xl font-bold uppercase md:text-2xl lg:text-3xl md:w-full lg:w-[90%] xl:w-[70%] xl:text-4xl  ">
+    <motion.section className="w-full h-screen overflow-hidden phone-bg md:px-40">
+      <motion.div className="h-full flex flex-col items-center justify-end pb-10 text-center gap-3 z-20 lg:gap-5 text-white w-[85%] mx-auto md:mx-0 md:justify-center lg:py-32 md:items-start md:text-left lg:w-[50%]">
+        <motion.h1
+          className="text-xl font-bold uppercase md:text-2xl lg:text-3xl md:w-full lg:w-[90%] xl:w-[70%]"
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8, delay: 0.4 }}
+        >
           AVEC VOUS JUSQU’A LA DERNIèRE PIERRE!
-        </h1>
-        <p className="text-sm md:text-sm  md:w-[90%]  lg:text-lg shadow-lg opacity-80 xl:w-[70%]">
+        </motion.h1>
+        <motion.p
+          className="text-sm md:text-sm md:w-[90%] lg:text-lg shadow-lg opacity-80 xl:w-[70%]"
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8, delay: 0.6 }}
+        >
           Bienvenue sur le site de Guirri-bat, votre entreprise de démolition,
           rénovation et terrassement à Birkhadem. Nous vous proposons des
           prestations de qualité, adaptées à vos projets et à votre budget.
-        </p>
-        <button className="bg-yellow-primary px-10 py-2 text-lg text-black font-bold rounded-lg md:text-xl hover:bg-white duration-300">
+        </motion.p>
+        <motion.button
+          className="bg-yellow-primary px-10 py-2 text-lg text-black font-bold rounded-lg md:text-xl hover:bg-white duration-300"
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8, delay: 0.8 }}
+        >
           FAIRE UN PROJET
-        </button>
-      </div>
-    </section>
+        </motion.button>
+      </motion.div>
+    </motion.section>
   );
 };
 
--- src/components/shared/CTABtn.jsx
+++ src/components/shared/CTABtn.jsx
@@ -1,10 +1,22 @@
+import { useState } from "react";
 import ctaIcon from "../../assets/imgs/ctaIcon.svg";
 
 const CTABtn = () => {
+  const [showText, setShowText] = useState(false);
   return (
-    <div className="fixed bottom-10 cursor-pointer right-32 bg-yellow-primary w-[70px] h-[70px]  flex items-center justify-center rounded-full">
+    <div
+      onClick={() => setShowText(!showText)}
+      className="fixed bottom-10 cursor-pointer z-20 right-3 bg-yellow-primary w-[70px] h-[70px]  flex items-center justify-center rounded-full"
+    >
       <div className="relative h-[70%] w-[70%]">
         <img className="object-contain w-full h-full" src={ctaIcon} alt="cta" />
+      </div>
+      <div
+        className={`absolute ${
+          showText ? "opacity-100" : "opacity-0"
+        } duration-300 right-20 bg-yellow-primary w-[200px] py-2 text-center rounded-md`}
+      >
+        <span className="text-white capitalize">contact us</span>
       </div>
     </div>
   );
